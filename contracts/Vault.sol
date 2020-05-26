fe5e9ed4b5c3f8ac812b2cb4360234e3b91f294f
Khoa Phan <kp@conclave.dev>
2020-05-26 19:59:59 -0400

    Add a unit test for creating a new Vault instance and checking its address

---
+++ contracts/Vault.sol
@@ -0,0 +1,9 @@
+// contracts/Product.sol
+pragma solidity ^0.5.0;
+
+import '@openzeppelin/upgrades/contracts/Initializable.sol';
+
+
+contract Vault is Initializable {
+  function initialize() public initializer {}
+}
---
+++ test/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  defaultSender: '0x57c445eaea6b8782b75a50e2069fc209386541f1'
+};
