70dc2548891ce3d933786cc75a63b0f5342ee97d
Khoa Phan <kp@conclave.dev>
2020-05-26 21:07:13 -0400

    Run prettier and remove unused typescript rules from eslint config

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.0;
 
 import '@openzeppelin/upgrades/contracts/Initializable.sol';
 
-
 contract Vault is Initializable {
   function initialize() public initializer {}
 }
--- contracts/VaultFactory.sol
+++ contracts/VaultFactory.sol
@@ -1,27 +1,26 @@
 // contracts/Factory.sol
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/upgrades/contracts/Initializable.sol";
-import "@openzeppelin/upgrades/contracts/application/App.sol";
+import '@openzeppelin/upgrades/contracts/Initializable.sol';
+import '@openzeppelin/upgrades/contracts/application/App.sol';
+
 
 contract VaultFactory is Initializable {
-    App private app;
+  App private app;
 
-    event InstanceCreated(address);
+  event InstanceCreated(address);
 
-    function initialize(App _app) public initializer {
-      app = _app;
-    }
+  function initialize(App _app) public initializer {
+    app = _app;
+  }
 
-    function createInstance(bytes memory _data) public {
-        string memory packageName = "autonomous-voter";
-        string memory contractName = "Vault";
-        address admin = msg.sender;
+  function createInstance(bytes memory _data) public {
+    string memory packageName = 'autonomous-voter';
+    string memory contractName = 'Vault';
+    address admin = msg.sender;
 
-        address vault = address(
-            app.create(packageName, contractName, admin, _data)
-        );
+    address vault = address(app.create(packageName, contractName, admin, _data));
 
-        emit InstanceCreated(vault);
-    }
+    emit InstanceCreated(vault);
+  }
 }
