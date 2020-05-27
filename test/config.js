b60e83622c5c7ae5556247dbfd1e7565fe9a971a
Khoa Phan <kp@conclave.dev>
2020-05-27 11:40:58 -0400

    Add unit test for creating a Vault contract instance

--- test/config.js
+++ test/config.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+
+const { APP_CONTRACT_ADDRESS, DEFAULT_SENDER_ADDRESS } = process.env;
+
 module.exports = {
-  defaultSender: '0x5409ed021d9299bf6814279a6a1411a7e866a631'
+  APP_CONTRACT_ADDRESS,
+  DEFAULT_SENDER_ADDRESS
 };
