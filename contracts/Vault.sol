d29468b91eee849a71067a51cfa86fa54095e3b1
Eko Mirhard <eko.mirhard@gmail.com>
2020-05-28 00:36:34 +0700

    Update Vault.sol

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.5.0;
 
 import '@openzeppelin/upgrades/contracts/Initializable.sol';
+import './celo/common/UsingRegistry.sol';
 
-contract Vault is Initializable {
-  function initialize() public initializer {}
+contract Vault is Initializable, UsingRegistry {
+  function initialize(address registryAddress) public initializer {
+    setRegistry(registryAddress);
+  }
 }
