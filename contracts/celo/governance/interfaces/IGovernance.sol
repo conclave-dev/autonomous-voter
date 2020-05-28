c32d60bf701e9515fcadc6c7af9f6f4cb57742a6
Khoa Phan <kp@conclave.dev>
2020-05-28 12:28:23 -0400

    Remove local version of Celo interface contracts (re-added after merge)

--- contracts/celo/governance/interfaces/IGovernance.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.5.3;
-
-interface IGovernance {
-  function isVoting(address) external view returns (bool);
-}
--- contracts/celo/stability/interfaces/IExchange.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.3;
-
-interface IExchange {
-  function exchange(uint256, uint256, bool) external returns (uint256);
-  function setUpdateFrequency(uint256) external;
-  function getBuyTokenAmount(uint256, bool) external view returns (uint256);
-  function getSellTokenAmount(uint256, bool) external view returns (uint256);
-  function getBuyAndSellBuckets(bool) external view returns (uint256, uint256);
-}
