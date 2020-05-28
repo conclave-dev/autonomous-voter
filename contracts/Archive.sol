fd92aae11f2b88b2a17dd768df123c87f491dac5
Khoa Phan <kp@conclave.dev>
2020-05-28 16:55:19 -0400

    Add setVaultOwner method and update tests

--- contracts/Archive.sol
+++ contracts/Archive.sol
@@ -5,7 +5,13 @@ import '@openzeppelin/upgrades/contracts/Initializable.sol';
 import '@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol';
 
 contract Archive is Initializable, Ownable {
+  address public vaultFactory;
+
   function initialize(address _owner) public initializer {
     Ownable.initialize(_owner);
+  }
+
+  function setVaultFactory(address _vaultFactory) public onlyOwner {
+    vaultFactory = _vaultFactory;
   }
 }
--- test/Archive.test.js
+++ test/Archive.test.js
@@ -1,7 +1,10 @@
 const { contract } = require('@openzeppelin/test-environment');
-const { expect } = require('chai');
+const chai = require('chai');
 const { DEFAULT_SENDER_ADDRESS } = require('./config');
 
+chai.use(require('chai-as-promised'));
+
+const expect = chai.expect;
 const Archive = contract.fromArtifact('Archive');
 
 describe('Archive', function () {
@@ -19,5 +22,28 @@ describe('Archive', function () {
     const archiveOwner = (await this.archive.owner()).toLowerCase();
 
     expect(archiveOwner).to.equal(DEFAULT_SENDER_ADDRESS);
+  });
+
+  describe('Access Control', function () {
+    it('should not change vaultFactory if not owner', async function () {
+      this.vaultFactory = await contract.fromArtifact('VaultFactory').new({ from: DEFAULT_SENDER_ADDRESS });
+      const nonOwnerAddress = '0x57c445eaea6b8782b75a50e2069fc209386541f1';
+
+      await expect(
+        this.archive.setVaultFactory.sendTransaction(this.vaultFactory.address, {
+          from: nonOwnerAddress
+        })
+      ).to.be.rejectedWith(Error);
+    });
+
+    it('should change vaultFactory if owner', async function () {
+      expect(await this.archive.vaultFactory()).to.equal('0x0000000000000000000000000000000000000000');
+
+      this.archive.setVaultFactory.sendTransaction(this.vaultFactory.address, {
+        from: DEFAULT_SENDER_ADDRESS
+      });
+
+      expect(await this.archive.vaultFactory()).to.equal(this.vaultFactory.address);
+    });
   });
 });
