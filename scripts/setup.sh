fa7d7920a6237db116c6ef52ddecd647819e7148
Khoa Phan <kp@conclave.dev>
2020-05-28 12:54:01 -0400

    Pin @celo/ganache-core package to commit hash and use https when cloning repo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,6 @@
 
 [ ! -d "node_modules/" ] && npm install
 
-[ ! -d "node_modules/celo-monorepo" ] && git clone --single-branch --branch baklava git@github.com:celo-org/celo-monorepo.git node_modules/celo-monorepo/
+[ ! -d "node_modules/celo-monorepo" ] && git clone --single-branch --branch baklava https://github.com/celo-org/celo-monorepo node_modules/celo-monorepo/
 
 node ./scripts/setup.js
