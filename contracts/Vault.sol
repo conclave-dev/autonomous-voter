7606e482899f9b20f6a591984bd23fac05cbf111
Eko Mirhard <eko.mirhard@gmail.com>
2020-05-29 22:40:50 +0700

    Add deposit method

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -5,8 +5,14 @@ import '@openzeppelin/upgrades/contracts/Initializable.sol';
 import './celo/common/UsingRegistry.sol';
 
 contract Vault is UsingRegistry {
+  event UserDeposit(uint256);
+
   function initialize(address registryAddress) public initializer {
     UsingRegistry.initializeRegistry(msg.sender, registryAddress);
     require(getAccounts().createAccount(), "Failed to register vault account");
+  }
+
+  function deposit() public payable {
+    emit UserDeposit(msg.value);
   }
 }
