5a9eeb1a162ab5702cd108bfea4f2da4a09eb652
Khoa Phan <kp@conclave.dev>
2020-05-29 13:12:32 -0400

    Prettify contract files

--- contracts/Archive.sol
+++ contracts/Archive.sol
@@ -1,30 +1,30 @@
-// contracts/Vault.sol
+// contracts/Archive.sol
 pragma solidity ^0.5.0;
 
-import '@openzeppelin/upgrades/contracts/Initializable.sol';
-import '@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol';
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 
 
 contract Archive is Initializable, Ownable {
-  address public vaultFactory;
-  mapping(address => address) public vaults;
+    address public vaultFactory;
+    mapping(address => address) public vaults;
 
-  event VaultFactorySet(address);
-  event VaultUpdated(address);
+    event VaultFactorySet(address);
+    event VaultUpdated(address);
 
-  function initialize(address _owner) public initializer {
-    Ownable.initialize(_owner);
-  }
+    function initialize(address _owner) public initializer {
+        Ownable.initialize(_owner);
+    }
 
-  function setVaultFactory(address _vaultFactory) public onlyOwner {
-    vaultFactory = _vaultFactory;
+    function setVaultFactory(address _vaultFactory) public onlyOwner {
+        vaultFactory = _vaultFactory;
 
-    emit VaultFactorySet(vaultFactory);
-  }
+        emit VaultFactorySet(vaultFactory);
+    }
 
-  function updateVault(address vault) public {
-    vaults[msg.sender] = vault;
+    function updateVault(address vault) public {
+        vaults[msg.sender] = vault;
 
-    emit VaultUpdated(msg.sender);
-  }
+        emit VaultUpdated(msg.sender);
+    }
 }
--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,12 +1,16 @@
 // contracts/Vault.sol
 pragma solidity ^0.5.0;
 
-import '@openzeppelin/upgrades/contracts/Initializable.sol';
-import './celo/common/UsingRegistry.sol';
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "./celo/common/UsingRegistry.sol";
+
 
 contract Vault is UsingRegistry {
-  function initialize(address registryAddress) public initializer {
-    UsingRegistry.initializeRegistry(msg.sender, registryAddress);
-    require(getAccounts().createAccount(), "Failed to register vault account");
-  }
+    function initialize(address registryAddress) public initializer {
+        UsingRegistry.initializeRegistry(msg.sender, registryAddress);
+        require(
+            getAccounts().createAccount(),
+            "Failed to register vault account"
+        );
+    }
 }
