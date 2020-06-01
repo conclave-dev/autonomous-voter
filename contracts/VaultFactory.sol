6ad2fd7472fd83edf05a5298e8c97085092fef0c
Khoa Phan <kp@conclave.dev>
2020-06-01 10:05:45 -0400

    Add vault to Archive after it is created

--- contracts/VaultFactory.sol
+++ contracts/VaultFactory.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "@openzeppelin/upgrades/contracts/application/App.sol";
+import "./interfaces/IArchive.sol";
 
 
 contract VaultFactory is Initializable {
     App private app;
+    IArchive public archive;
 
     event InstanceCreated(address);
+    event InstanceArchived(address, address);
 
-    function initialize(App _app) public initializer {
+    function initialize(App _app, IArchive _archive) public initializer {
         app = _app;
+        archive = _archive;
     }
 
     function createInstance(bytes memory _data) public {
@@ -24,5 +28,9 @@ contract VaultFactory is Initializable {
         );
 
         emit InstanceCreated(vault);
+
+        archive.updateVault(vault, admin);
+
+        emit InstanceArchived(vault, admin);
     }
 }
---
+++ contracts/interfaces/IArchive.sol
@@ -0,0 +1,7 @@
+// contracts/interfaces/IArchive.sol
+pragma solidity ^0.5.0;
+
+
+interface IArchive {
+    function updateVault(address vault, address vaultAdmin) external;
+}
