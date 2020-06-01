2e5d40bb6355478c4f01b712060069f50d53a7ac
Khoa Phan <kp@conclave.dev>
2020-06-01 10:03:15 -0400

    Update ganache config to create two test addresses

--- ganache/baklava.sh
+++ ganache/baklava.sh
@@ -5,6 +5,7 @@ export MNEMONIC="concert load couple harbor equip island argue ramp clarify fenc
 export NETWORK_ID="40120"
 export GAS_LIMIT="20000000"
 export UNLOCK_PRIMARY="0x5409ed021d9299bf6814279a6a1411a7e866a631"
-export UNLOCK_SECONDARY="0x57c445eaea6b8782b75a50e2069fc209386541f1"
+export UNLOCK_SECONDARY="0x48fF477891eCcd5177Ec8d66210EC2308fAc6eD6"
 
-ganache-cli --fork $FORK --mnemonic $MNEMONIC --networkId $NETWORK_ID --gasLimit $GAS_LIMIT --unlock $UNLOCK_PRIMARY --unlock $UNLOCK_SECONDARY
+ganache-cli --fork $FORK --networkId $NETWORK_ID --gasLimit $GAS_LIMIT \
+--accounts 2 --mnemonic $MNEMONIC --unlock $UNLOCK_PRIMARY --unlock $UNLOCK_SECONDARY
