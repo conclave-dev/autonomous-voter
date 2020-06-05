cd5f23bde444a68300efcc428782ace2a296b4f9
Khoa Phan <kp@conclave.dev>
2020-06-05 11:23:23 -0400

    Update Archive migration to conditionally initialize with an owner

--- migrations/2_deploy_archive.js
+++ migrations/2_deploy_archive.js
@@ -1,6 +1,16 @@
 const { deployContract } = require('./util');
 
 module.exports = (deployer, networkName, accounts) =>
-  deployer.then(async () => {
-    await deployContract('Archive', networkName, accounts[0]);
-  });
+  deployer
+    .then(async () => {
+      await deployContract('Archive', networkName, accounts[0]);
+    })
+    .then(async () => {
+      const archive = await artifacts.require('Archive').deployed();
+
+      if (await archive.owner()) {
+        return;
+      }
+
+      await archive.initialize(accounts[0]);
+    });
