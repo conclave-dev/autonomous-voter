841300b54504fbae65384305e8388bc93157be30
Khoa Phan <kp@conclave.dev>
2020-06-05 16:00:46 -0400

    Update Archive test

--- test/Archive.test.js
+++ test/Archive.test.js
@@ -1,55 +1,62 @@
-const { expect } = require('./setup');
+const { expect, assert } = require('chai').use(require('chai-as-promised'));
+const { encodeCall } = require('@openzeppelin/upgrades');
+const BigNumber = require('bignumber.js');
+const { primarySenderAddress, secondarySenderAddress, registryContractAddress } = require('../config');
 
-describe('Archive', function () {
-  describe('Initialize', function () {
-    it('should initialize', function () {
-      expect(typeof this.archive.address).to.equal('string');
-      expect(this.archive.address.length).to.equal(42);
-      expect(this.archive.address).to.not.equal(this.address.zero);
-    });
+const { setupLoader } = require('@openzeppelin/contract-loader');
 
-    it('should have an owner', async function () {
-      const owner = await this.archive.owner();
+const loader = setupLoader({
+  provider: 'http://3.230.69.118:8545',
+  defaultSender: primarySenderAddress,
+  defaultGas: '20000000',
+  defaultGasPrice: '100000000000'
+});
 
-      expect(owner).to.not.equal(this.address.secondary);
-      expect(owner.toLowerCase()).to.equal(this.address.primary.toLowerCase());
-    });
-  });
+const Archive = loader.truffle.fromArtifact('Archive', '0xa426709C243e0E077Ff26Bf18656155f40d6CD2E');
+const VaultFactory = loader.truffle.fromArtifact('VaultFactory', '0x56B5FE5E5aCBa103E716d04ba6bE4Ccfd6B7850E');
 
-  describe('Ownership', function () {
-    it('should not set vaultFactory if not owner', async function () {
-      await expect(
-        this.archive.setVaultFactory(this.vaultFactory.address, { from: this.address.secondary })
-      ).to.be.rejectedWith(Error);
-    });
+describe('Archive', () => {
+  describe('initialize(address _owner)', () => {
+    it('should initialize with an owner', async () => {
+      const owner = await Archive.owner();
 
-    it('should set vaultFactory if owner', async function () {
-      const { logs } = await this.archive.setVaultFactory(this.vaultFactory.address, this.defaultTx);
-      const { event, args } = logs[0];
+      if (owner === '0x0000000000000000000000000000000000000000') {
+        await Archive.initialize(primarySenderAddress);
+      } else {
+        await expect(Archive.initialize(primarySenderAddress)).to.be.rejectedWith(Error);
+      }
 
-      expect(await this.archive.vaultFactory()).to.equal(this.vaultFactory.address);
-      expect(args[0]).to.equal(this.vaultFactory.address); // Check event emitted with correct value
-      expect(event).to.equal('VaultFactorySet');
+      assert.equal(await Archive.owner(), primarySenderAddress, 'Owner does not match sender');
     });
   });
 
-  describe('Methods', function () {
-    describe('updateVault(address vault)', function () {
-      it('should not set a vault if not vault factory', async function () {
-        await expect(
-          this.archive.updateVault(this.vault.address, this.address.secondary, { from: this.address.secondary })
-        ).to.be.rejectedWith(
-          'Returned error: VM Exception while processing transaction: revert Sender is not vault factory -- Reason given: Sender is not vault factory.'
-        );
-      });
+  describe('setVaultFactory(address _vaultFactory)', () => {
+    it('should not allow a non-owner to set vaultFactory', async () => {
+      await expect(Archive.setVaultFactory(VaultFactory.address, { from: secondarySenderAddress })).to.be.rejectedWith(
+        Error
+      );
+    });
 
-      it('should set a vault if vault factory', async function () {
-        expect(await this.archive.vaults(this.address.secondary)).to.equal(this.address.zero);
+    it('should allow its owner to set vaultFactory', async () => {
+      await Archive.setVaultFactory(VaultFactory.address);
+      assert.equal(await Archive.vaultFactory(), VaultFactory.address, 'Owner did not set vault factory');
+    });
+  });
 
-        const _vault = await this.createVault(this.address.secondary, this.archive, this.vaultFactory);
+  describe('updateVault(address vault, address vaultAdmin)', () => {
+    it('should initialize vault', async () => {
+      const { logs: events } = await VaultFactory.createInstance(
+        encodeCall('initializeVault', ['address', 'address'], [registryContractAddress, primarySenderAddress]),
+        {
+          from: primarySenderAddress,
+          value: new BigNumber(1).multipliedBy('1e17')
+        }
+      );
+      const [instanceCreated, adminCreated, instanceArchived] = events;
+      const vault = loader.truffle.fromArtifact('Vault', instanceCreated.args[0]);
 
-        expect(await this.archive.vaults(this.address.secondary)).to.equal(_vault.address);
-      });
+      assert.equal(vault.admin() === adminCreated.args[0], 'Vault was not initialized with correct proxy admin');
+      assert.equal(vault.owner() === instanceArchived.args[1], 'Vault was not initialized with correct owner');
     });
   });
 });
