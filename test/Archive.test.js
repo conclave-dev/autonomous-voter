84ef5f431d95db1a51335a81bb0a616eae91e00e
Khoa Phan <kp@conclave.dev>
2020-06-07 18:32:47 -0400

    Update contract tests to reflect changes

--- test/Archive.test.js
+++ test/Archive.test.js
@@ -1,44 +1,44 @@
 const { encodeCall } = require('@openzeppelin/upgrades');
 const BigNumber = require('bignumber.js');
-const { assert, expect, loader, contracts } = require('./setup');
+const { assert, expect, contracts } = require('./setup');
 const { primarySenderAddress, secondarySenderAddress, registryContractAddress } = require('../config');
 
-const { Archive, VaultFactory } = contracts;
-
 describe('Archive', () => {
+  before(async () => {
+    this.archive = await contracts.Archive.deployed();
+    this.vaultFactory = await contracts.VaultFactory.deployed();
+  });
+
   describe('initialize(address _owner)', () => {
     it('should initialize with an owner', async () => {
-      assert.equal(await Archive.owner(), primarySenderAddress, 'Owner does not match sender');
+      assert.equal(await this.archive.owner(), primarySenderAddress, 'Owner does not match sender');
     });
   });
 
   describe('setVaultFactory(address _vaultFactory)', () => {
     it('should not allow a non-owner to set vaultFactory', async () => {
-      await expect(Archive.setVaultFactory(VaultFactory.address, { from: secondarySenderAddress })).to.be.rejectedWith(
-        Error
-      );
+      await expect(
+        this.archive.setVaultFactory(this.vaultFactory.address, { from: secondarySenderAddress })
+      ).to.be.rejectedWith(Error);
     });
 
     it('should allow its owner to set vaultFactory', async () => {
-      await Archive.setVaultFactory(VaultFactory.address);
-      await expect(Archive.setVaultFactory(VaultFactory.address, { from: secondarySenderAddress })).to.be.rejectedWith(
-        Error
-      );
-      assert.equal(await Archive.vaultFactory(), VaultFactory.address, 'Owner did not set vault factory');
+      await this.archive.setVaultFactory(this.vaultFactory.address);
+
+      assert.equal(await this.archive.vaultFactory(), this.vaultFactory.address, 'Owner did not set vault factory');
     });
   });
 
   describe('updateVault(address vault, address vaultAdmin)', () => {
     it('should initialize vault', async () => {
-      const { logs: events } = await VaultFactory.createInstance(
+      const { logs: events } = await this.vaultFactory.createInstance(
         encodeCall('initializeVault', ['address', 'address'], [registryContractAddress, primarySenderAddress]),
         {
-          from: primarySenderAddress,
           value: new BigNumber(1).multipliedBy('1e17')
         }
       );
       const [instanceCreated, , instanceArchived] = events;
-      const vault = loader.truffle.fromArtifact('Vault', instanceCreated.args[0]);
+      const vault = await contracts.Vault.at(instanceCreated.args[0]);
 
       assert.equal(await vault.owner(), instanceArchived.args[1], 'Vault was not initialized with correct owner');
     });
