d4516ade33990b294128ac7b475bca0cb3eccda4
Eko Mirhard <eko.mirhard@gmail.com>
2020-06-08 19:32:13 +0700

    Update 4_app.js

--- migrations/4_app.js
+++ migrations/4_app.js
@@ -5,11 +5,11 @@ const Strategy = artifacts.require('Strategy');
 module.exports = async (deployer) => {
   await deployer.deploy(App, { overwrite: false });
 
-  // const app = await App.deployed();
+  const app = await App.deployed();
   const { address: vaultAddress } = await Vault.deployed();
   const { address: strategyAddress } = await Strategy.deployed();
 
-  // await app.initialize();
+  await app.initialize();
   await app.setImplementation('Vault', vaultAddress);
   await app.setImplementation('Strategy', strategyAddress);
 };
