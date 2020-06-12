686d4304cb5d1e36f053d8935428d74ea6c42c7e
Eko Mirhard <eko.mirhard@gmail.com>
2020-06-13 00:47:03 +0700

    Fix updated migrations

--- migrations/10_set_app_contract_factories.js
+++ migrations/10_set_app_contract_factories.js
@@ -1,14 +1,14 @@
 const App = artifacts.require('App');
 const VaultFactory = artifacts.require('VaultFactory');
-const StrategyFactory = artifacts.require('StrategyFactory');
+const VaultManagerFactory = artifacts.require('VaultManagerFactory');
 
 module.exports = async (deployer) => {
   deployer.then(async () => {
     const app = await App.deployed();
     const vaultFactory = await VaultFactory.deployed();
-    const strategyFactory = await StrategyFactory.deployed();
+    const vaultManagerFactory = await VaultManagerFactory.deployed();
 
     await app.setContractFactory('Vault', vaultFactory.address);
-    await app.setContractFactory('Strategy', strategyFactory.address);
+    await app.setContractFactory('Strategy', vaultManagerFactory.address);
   });
 };
