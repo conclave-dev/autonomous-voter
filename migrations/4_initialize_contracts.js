5577ce43ef996e4c31b9c7f383734222c425c7c5
Khoa Phan <kp@conclave.dev>
2020-06-13 12:51:31 -0400

    Catch initializer SIGTERM

--- migrations/4_initialize_contracts.js
+++ migrations/4_initialize_contracts.js
@@ -1,8 +1,10 @@
+const Promise = require('bluebird');
+const { registryContractAddress } = require('../config');
+
 const App = artifacts.require('App');
 const Archive = artifacts.require('Archive');
 const VaultFactory = artifacts.require('VaultFactory');
 const VaultManagerFactory = artifacts.require('VaultManagerFactory');
-const { registryContractAddress } = require('../config');
 
 module.exports = (deployer) =>
   deployer.then(async () => {
@@ -10,9 +12,24 @@ module.exports = (deployer) =>
     const archive = await Archive.deployed();
     const vaultFactory = await VaultFactory.deployed();
     const vaultManagerFactory = await VaultManagerFactory.deployed();
+    const contractInitializers = [
+      { contract: 'App', fn: async () => await app.initialize() },
+      { contract: 'Archive', fn: async () => await archive.initialize(registryContractAddress) },
+      {
+        contract: 'VaultFactory',
+        fn: async () => await vaultFactory.initialize(app.address, archive.address)
+      },
+      {
+        contract: 'VaultManagerFactory',
+        fn: async () => await vaultManagerFactory.initialize(app.address, archive.address)
+      }
+    ];
 
-    await app.initialize();
-    await archive.initialize(registryContractAddress);
-    await vaultFactory.initialize(app.address, archive.address);
-    await vaultManagerFactory.initialize(app.address, archive.address);
+    await Promise.each(contractInitializers, async ({ contract, fn }) => {
+      try {
+        await fn();
+      } catch (err) {
+        console.error(`Error calling ${contract} initializer: ${err}`);
+      }
+    });
   });
