d00bf16ebe9f6c44bafd737424feb873df745450
Khoa Phan <kp@conclave.dev>
2020-06-13 12:45:55 -0400

    Disable unnecessary contract deployments

--- migrations/2_linked_list_libraries.js
+++ migrations/2_linked_list_libraries.js
@@ -4,10 +4,10 @@ const VaultManager = artifacts.require('VaultManager');
 const Archive = artifacts.require('Archive');
 
 module.exports = async (deployer) => {
-  await deployer.deploy(LinkedList);
+  await deployer.deploy(LinkedList, { overwrite: false });
   await deployer.link(LinkedList, AddressLinkedList);
 
-  await deployer.deploy(AddressLinkedList);
+  await deployer.deploy(AddressLinkedList, { overwrite: false });
   await deployer.link(AddressLinkedList, VaultManager);
   await deployer.link(AddressLinkedList, Archive);
 };
--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -1,13 +1,14 @@
+const Promise = require('bluebird');
+
 const Vault = artifacts.require('Vault');
 const VaultManager = artifacts.require('VaultManager');
 const App = artifacts.require('App');
 const Archive = artifacts.require('Archive');
 const VaultFactory = artifacts.require('VaultFactory');
 const VaultManagerFactory = artifacts.require('VaultManagerFactory');
-const Promise = require('bluebird');
 
 const contracts = [Vault, VaultManager, App, Archive, VaultFactory, VaultManagerFactory];
 
 module.exports = async (deployer) => {
-  await Promise.each(contracts, async (contract) => await deployer.deploy(contract));
+  await Promise.each(contracts, async (contract) => await deployer.deploy(contract, { overwrite: false }));
 };
