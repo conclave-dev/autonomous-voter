71565d510c25a4c3491178bf97cea71c58df7b99
Khoa Phan <kp@conclave.dev>
2020-06-13 12:59:31 -0400

    Add checks to avoid re-setting archive state with duplicate values

--- migrations/5_set_archive_state.js
+++ migrations/5_set_archive_state.js
@@ -2,13 +2,19 @@ const Archive = artifacts.require('Archive');
 const VaultFactory = artifacts.require('VaultFactory');
 const VaultManagerFactory = artifacts.require('VaultManagerFactory');
 
-module.exports = (deployer) => {
+module.exports = (deployer) =>
   deployer.then(async () => {
     const archive = await Archive.deployed();
     const vaultFactory = await VaultFactory.deployed();
     const vaultManagerFactory = await VaultManagerFactory.deployed();
+    const hasVaultFactory = (await archive.vaultFactory()) === vaultFactory.address;
+    const hasVaultManagerFactory = (await archive.vaultManagerFactory()) === vaultManagerFactory.address;
 
-    await archive.setVaultFactory(vaultFactory.address);
-    await archive.setVaultManagerFactory(vaultManagerFactory.address);
+    if (!hasVaultFactory) {
+      await archive.setVaultFactory(vaultFactory.address);
+    }
+
+    if (!hasVaultManagerFactory) {
+      await archive.setVaultManagerFactory(vaultManagerFactory.address);
+    }
   });
-};
