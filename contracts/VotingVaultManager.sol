a5de80a92a504a3f2ea108684d5fa63c59e3acdd
Khoa Phan <kp@conclave.dev>
2020-06-18 17:30:24 -0400

    Remove redundant initialize fn and change vault from address to contract for type safety

--- contracts/VotingVaultManager.sol
+++ contracts/VotingVaultManager.sol
@@ -3,34 +3,16 @@ pragma solidity ^0.5.8;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 import "./VaultManager.sol";
-import "./Vault.sol";
 
 contract VotingVaultManager is VaultManager {
-    function initialize(
-        Archive archive_,
-        address owner_,
-        address admin,
-        uint256 sharePercentage,
-        uint256 minimumRequirement
-    ) public initializer {
-        VaultManager.initialize(
-            archive_,
-            owner_,
-            admin,
-            sharePercentage,
-            minimumRequirement
-        );
-        Ownable.initialize(owner_);
-    }
-
     function vote(
-        address vault,
+        Vault vault,
         address group,
         uint256 amount,
         address adjacentGroupWithLessVotes,
         address adjacentGroupWithMoreVotes
-    ) public onlyOwner onlyManagedVault(vault) {
-        Vault(vault).vote(
+    ) public onlyOwner onlyManagedVault(address(vault)) {
+        vault.vote(
             group,
             amount,
             adjacentGroupWithLessVotes,
@@ -39,15 +21,15 @@ contract VotingVaultManager is VaultManager {
     }
 
     function revokePending(
-        address vault,
+        Vault vault,
         address group,
         uint256 amount,
         address adjacentGroupWithLessVotes,
         address adjacentGroupWithMoreVotes,
         uint256 accountGroupIndex
-    ) public onlyOwner onlyManagedVault(vault) {
+    ) public onlyOwner onlyManagedVault(address(vault)) {
         // Validates group and revoke amount (cannot be zero or greater than pending votes)
-        Vault(vault).revokePending(
+        vault.revokePending(
             group,
             amount,
             adjacentGroupWithLessVotes,
