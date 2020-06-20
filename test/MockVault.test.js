5a2e08dfa4a36a91f3d7353ad012a188fe9d34c4
Khoa Phan <kp@conclave.dev>
2020-06-20 14:21:13 -0400

    Test rewards calculation with small numbers

--- test/MockVault.test.js
+++ test/MockVault.test.js
@@ -2,6 +2,14 @@ const BigNumber = require('bignumber.js');
 const { assert } = require('./setup');
 const { primarySenderAddress } = require('../config');
 
+const mockCalculateVotingManagerRewards = (activeVotes, activeVotesWithoutRewards, rewardSharePercentage) => {
+  // Vault calculateVotingManagerRewards logic in JS
+  const rewards = activeVotes - activeVotesWithoutRewards;
+  const rewardsPercent = rewards / 100;
+
+  return Math.floor(rewardsPercent * rewardSharePercentage);
+};
+
 describe('MockVault', function () {
   it('should calculate the voting manager rewards (random numbers)', async function () {
     const mockActiveVotes = Math.floor(Math.random() * 1e8) + 1e6;
@@ -21,10 +29,45 @@ describe('MockVault', function () {
 
     await this.mockVault.setRewardSharePercentage(mockRewardSharePercentage);
 
-    // Vault calculateVotingManagerRewards logic
-    const rewards = mockActiveVotes - mockActiveVotesWithoutRewards;
-    const rewardsPercent = rewards / 100;
-    const expectedManagerReward = Math.floor(rewardsPercent * mockRewardSharePercentage); // Round down, as decimals truncated
+    const expectedManagerReward = mockCalculateVotingManagerRewards(
+      mockActiveVotes,
+      mockActiveVotesWithoutRewards,
+      mockRewardSharePercentage
+    );
+    const actualManagerReward = new BigNumber(await this.mockVault.calculateVotingManagerRewards(primarySenderAddress));
+
+    return assert.equal(
+      actualManagerReward.isEqualTo(expectedManagerReward),
+      true,
+      'Expected and actual rewards were different'
+    );
+  });
+
+  it('should calculate the voting manager rewards (small numbers)', async function () {
+    const mockActiveVotes = 120;
+    const mockActiveVotesWithoutRewards = 100;
+    const mockRewardSharePercentage = (await this.mockVault.votingManager()).rewardSharePercentage;
+
+    await this.mockElection.setActiveVotesForGroupByAccount(
+      primarySenderAddress,
+      this.mockVault.address,
+      mockActiveVotes
+    );
+
+    await this.mockVault.setActiveVotesWithoutRewardsForGroup(
+      primarySenderAddress,
+      await mockActiveVotesWithoutRewards
+    );
+
+    // When using SafeMath methods, the result would be 0 with the values above
+    // due to order of operations + decimal numbers being rounded down.
+    // Using Fixidity, we can hold off on the latter until the end, resulting in
+    // the return value being 1
+    const expectedManagerReward = mockCalculateVotingManagerRewards(
+      mockActiveVotes,
+      mockActiveVotesWithoutRewards,
+      mockRewardSharePercentage
+    );
     const actualManagerReward = new BigNumber(await this.mockVault.calculateVotingManagerRewards(primarySenderAddress));
 
     return assert.equal(
