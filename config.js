6726f4b37e94cbb41880dd0442a4ebaf601e89ba
Khoa Phan <kp@conclave.dev>
2020-06-26 16:32:35 -0400

    Reconfigure local testing environment

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   alfajoresRpcAPI: 'http://3.230.69.118:8545',
   baklavaRpcAPI: 'http://3.230.69.118:8547',
+  localRpcAPI: 'http://localhost:8545',
   primarySenderAddress: '0x876b74eDac7b5ecad82Ac3bf446c8E1503FEF37d',
   secondarySenderAddress: '0x4a8fC10F153BAa6f5F6A347E11B8aA00BbffD0a6',
   registryContractAddress: '0x000000000000000000000000000000000000ce10',
