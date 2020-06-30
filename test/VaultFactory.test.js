34f0b8b8fae11c7ab20aa465702d4b8393021915
Khoa Phan <kp@conclave.dev>
2020-06-30 15:03:07 -0400

    Change createInstance visibility to external and add tests

--- test/VaultFactory.test.js
+++ test/VaultFactory.test.js
@@ -15,4 +15,42 @@ describe('VaultFactory', function () {
       return assert.equal(await this.vaultFactory.archive(), this.archive.address);
     });
   });
+
+  describe('Methods ✅', function () {
+    it('should create an instance from a valid implementation and Celo Registry contract', function () {
+      return assert.isFulfilled(
+        this.vaultFactory.createInstance('Vault', this.registryContractAddress, {
+          value: new BigNumber('1e17')
+        })
+      );
+    });
+  });
+
+  describe('Methods 🛑', function () {
+    it('should not create an instance with an insufficient deposit amount', async function () {
+      const depositBelowMinimum = new BigNumber(await this.vaultFactory.MINIMUM_DEPOSIT()).minus(1);
+
+      return assert.isRejected(
+        this.vaultFactory.createInstance('Vault', this.registryContractAddress, {
+          value: depositBelowMinimum
+        })
+      );
+    });
+
+    it('should not create an instance from an invalid implementation', function () {
+      return assert.isRejected(
+        this.vaultFactory.createInstance('BadVault', this.registryContractAddress, {
+          value: new BigNumber('1e17')
+        })
+      );
+    });
+
+    it('should not create an instance from an invalid Celo Registry contract', function () {
+      return assert.isRejected(
+        this.vaultFactory.createInstance('Vault', this.zeroAddress, {
+          value: new BigNumber('1e17')
+        })
+      );
+    });
+  });
 });
