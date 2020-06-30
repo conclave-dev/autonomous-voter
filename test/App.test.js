3c42f45b80601aebc121155d76570895f83779c1
Eko Mirhard <eko.mirhard@gmail.com>
2020-07-01 00:26:19 +0700

    Update App.test.js

--- test/App.test.js
+++ test/App.test.js
@@ -3,13 +3,13 @@ const { assert } = require('./setup');
 const { registryContractAddress } = require('../config');
 
 describe('App', function () {
-  describe('initialize()', function () {
+  describe('State', function () {
     it('should initialize with an owner', async function () {
       return assert.equal(await this.app.owner(), this.primarySender, 'Owner does not match sender');
     });
   });
 
-  describe('setContractImplementation(string contractName, address implementation)', function () {
+  describe('Methods', function () {
     it('should allow its owner to set implementation', function () {
       assert.isRejected(
         this.app.setContractImplementation('Vault', this.vault.address, { from: this.secondarySender })
@@ -17,9 +17,7 @@ describe('App', function () {
 
       return assert.isFulfilled(this.app.setContractImplementation('Vault', this.vault.address));
     });
-  });
 
-  describe('setContractFactory(string contractName, address factory)', function () {
     it('should allow its owner to set factory', function () {
       assert.isRejected(
         this.app.setContractFactory('Vault', this.vaultFactory.address, { from: this.secondarySender })
@@ -27,9 +25,7 @@ describe('App', function () {
 
       return assert.isFulfilled(this.app.setContractFactory('Vault', this.vaultFactory.address));
     });
-  });
 
-  describe('create(string contractName, address admin, bytes data', function () {
     it('should allow authorized factories to create instances', function () {
       assert.isRejected(this.app.create('Vault', this.primarySender, '0x0'));
 
