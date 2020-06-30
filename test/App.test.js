8ceee004972fd7137def730d14b037d913a482f0
Eko Mirhard <eko.mirhard@gmail.com>
2020-07-01 00:43:48 +0700

    Update App.test.js

--- test/App.test.js
+++ test/App.test.js
@@ -9,31 +9,39 @@ describe('App', function () {
     });
   });
 
-  describe('Methods', function () {
-    it('should allow its owner to set implementation', function () {
-      assert.isRejected(
-        this.app.setContractImplementation('Vault', this.vault.address, { from: this.secondarySender })
-      );
-
+  describe('Methods - Success', function () {
+    it('should allow its owner to set contract implementation', function () {
       return assert.isFulfilled(this.app.setContractImplementation('Vault', this.vault.address));
     });
 
-    it('should allow its owner to set factory', function () {
-      assert.isRejected(
-        this.app.setContractFactory('Vault', this.vaultFactory.address, { from: this.secondarySender })
-      );
-
+    it('should allow its owner to set contract factory', function () {
       return assert.isFulfilled(this.app.setContractFactory('Vault', this.vaultFactory.address));
     });
 
-    it('should allow authorized factories to create instances', function () {
-      assert.isRejected(this.app.create('Vault', this.primarySender, '0x0'));
-
+    it('should allow authorized contract factories to create instances', function () {
       return assert.isFulfilled(
         this.vaultFactory.createInstance('Vault', registryContractAddress, {
           value: new BigNumber('1e17')
         })
       );
+    });
+  });
+
+  describe('Methods - Fail', function () {
+    it('should not allow non-owner to set contract implementation', function () {
+      return assert.isRejected(
+        this.app.setContractImplementation('Vault', this.vault.address, { from: this.secondarySender })
+      );
+    });
+
+    it('should not allow non-owner to set contract factory', function () {
+      return assert.isRejected(
+        this.app.setContractFactory('Vault', this.vaultFactory.address, { from: this.secondarySender })
+      );
+    });
+
+    it('should not allow unauthorized address to create instances', function () {
+      return assert.isRejected(this.app.create('Vault', this.primarySender, '0x0'));
     });
   });
 });
