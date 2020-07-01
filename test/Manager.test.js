415c66cce0715b9f33f2e6a99c19b4c90e5bd74a
Eko Mirhard <eko.mirhard@gmail.com>
2020-07-02 00:09:31 +0700

    Update Manager.test.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -3,6 +3,26 @@ const { assert } = require('./setup');
 
 describe('Manager', function () {
   describe('State', function () {
+    it('should initialize with a reference to the archive contract', async function () {
+      return assert.equal(await this.managerInstance.archive(), this.archive.address);
+    });
+
+    it('should initialize with a valid proxy admin', async function () {
+      return assert.notEqual(await this.managerInstance.proxyAdmin(), this.zeroAddress);
+    });
+
+    it('should have a linked list for storing vaults', async function () {
+      const vaults = await this.managerInstance.vaults();
+
+      assert.property(vaults, 'head');
+      assert.property(vaults, 'tail');
+      return assert.property(vaults, 'numElements');
+    });
+
+    it('should initialize with no registered vault', async function () {
+      return assert.isEmpty(await this.managerInstance.getVaults());
+    });
+
     it('should initialize with a manager commission', async function () {
       return assert.equal((await this.managerInstance.commission()).toString(), this.managerCommission);
     });
@@ -16,6 +36,12 @@ describe('Manager', function () {
   });
 
   describe('Methods ✅', function () {
+    it('should allow its owner to update the proxy admin', async function () {
+      await this.managerInstance.setProxyAdmin(this.secondarySender);
+
+      return assert.equal(await this.managerInstance.proxyAdmin(), this.secondarySender);
+    });
+
     it('should allow its owner to update the manager commission', async function () {
       this.managerCommission = '20';
 
@@ -37,8 +63,30 @@ describe('Manager', function () {
   });
 
   describe('Methods 🛑', function () {
+    it('should not allow non-owner account to update the proxy admin', function () {
+      return assert.isRejected(
+        this.managerInstance.setProxyAdmin(this.secondarySender, { from: this.secondarySender })
+      );
+    });
+
+    it('should not allow zero-address when updating the proxy admin', function () {
+      return assert.isRejected(this.managerInstance.setProxyAdmin(this.zeroAddress));
+    });
+
+    it('should not allow percentage value lower than 1 to update the manager commission', function () {
+      return assert.isRejected(this.managerInstance.setCommission(0));
+    });
+
+    it('should not allow percentage value higher than 100 to update the manager commission', function () {
+      return assert.isRejected(this.managerInstance.setCommission(101));
+    });
+
     it('should not allow non-owner account to update the manager commission', function () {
       return assert.isRejected(this.managerInstance.setCommission({ from: this.secondarySender }));
+    });
+
+    it('should not allow 0 as to update the minimum required balance', function () {
+      return assert.isRejected(this.managerInstance.setMinimumBalanceRequirement(0));
     });
 
     it('should not allow non-owner account to update the minimum required balance', function () {
