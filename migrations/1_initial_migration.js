68ab94f9e8afdefb58396f515b0060713657b2ac
Khoa Phan <kp@conclave.dev>
2020-07-08 19:25:45 -0400

    Deploy contracts in migrations if not-yet-deployed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,6 +2,8 @@ const Migrations = artifacts.require('Migrations');
 
 module.exports = async (deployer, network) => {
   const overwrite = network === 'local' ? true : false;
+  const deployMigrations =
+    overwrite || (await Migrations.deployed()).address === '0x0000000000000000000000000000000000000000';
 
-  await deployer.deploy(Migrations, { overwrite });
+  await deployer.deploy(Migrations, { overwrite: deployMigrations });
 };
--- migrations/2_linked_list_libraries.js
+++ migrations/2_linked_list_libraries.js
@@ -6,12 +6,16 @@ const VoteManager = artifacts.require('VoteManager');
 
 module.exports = async (deployer, network) => {
   const overwrite = network === 'local' ? true : false;
+  const deployLinkedList =
+    overwrite || (await LinkedList.deployed()).address === '0x0000000000000000000000000000000000000000';
+  const deployAddressLinkedList =
+    overwrite || (await AddressLinkedList.deployed()).address === '0x0000000000000000000000000000000000000000';
 
-  await deployer.deploy(LinkedList, { overwrite });
+  await deployer.deploy(LinkedList, { overwrite: deployLinkedList });
   await deployer.link(LinkedList, AddressLinkedList);
   await deployer.link(LinkedList, Vault);
 
-  await deployer.deploy(AddressLinkedList, { overwrite });
+  await deployer.deploy(AddressLinkedList, { overwrite: deployAddressLinkedList });
   await deployer.link(AddressLinkedList, Archive);
   await deployer.link(LinkedList, Vault);
   await deployer.link(AddressLinkedList, VoteManager);
