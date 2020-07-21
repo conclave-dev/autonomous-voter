15306830c1a0f8eab45201a0db6ac5b9b539d385
Eko Mirhard <eko.mirhard@gmail.com>
2020-07-21 22:46:46 +0700

    Base token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,6 @@
+// contracts/Token.sol
+pragma solidity ^0.5.8;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20//StandaloneERC20.sol";
+
+contract Token is StandaloneERC20 {}
