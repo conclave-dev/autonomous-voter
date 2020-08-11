0140b7aee211ed0f9e7a291cb444cd3a112524f9
Khoa Phan <kp@conclave.dev>
2020-08-11 14:17:29 -0400

    Add seed parameters vars and method for setting seedFreezeDuration

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -5,12 +5,23 @@ import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/StandaloneERC20.sol";
 
+import "./Vault.sol";
+
 /**
  * @title VM contract to manage token related functionalities
  *
  */
 contract Bank is Ownable, StandaloneERC20 {
     using SafeMath for uint256;
+
+    // # of seed AV tokens per Vault
+    uint256 constant seedCapacity = 1000;
+    // # of minted AV tokens per CELO
+    uint256 constant seedRatio = 1;
+
+    // Seconds AV tokens are frozen post-mint
+    // NOTE: Only modifiable seed parameter
+    uint256 seedFreezeDuration;
 
     struct LockedToken {
         uint256 amount;
@@ -30,11 +41,27 @@ contract Bank is Ownable, StandaloneERC20 {
         StandaloneERC20.initialize(name_, symbol_, decimals_, minters, pausers);
     }
 
-    // Placeholder method to allow minting tokens to those contributing
-    function seed() external payable {
+    /**
+     * @notice Sets the value of `seedFreezeDuration`
+     * @param duration Seconds AV tokens are frozen post-mint
+     */
+    function setSeedFreezeDuration(uint256 duration) public onlyOwner {
+        require(duration > 0, "Invalid duration");
+        seedFreezeDuration = duration;
+    }
+
+    /**
+     * @notice Mints AV tokens for a vault
+     * @param vault Vault contract deployed and owned by `msg.sender`
+     */
+    function seed(Vault vault) external payable {
+        require(msg.sender == vault.owner(), "Must be vault owner");
         require(msg.value > 0, "Invalid amount");
+
         // Currently set to mint on 1:1 basis
         _mint(msg.sender, msg.value);
+
+        // TODO: Store the minted amount + lockup (worked on by EM)
     }
 
     // Locks an account's token balance by adding it to `lockedTokens`
