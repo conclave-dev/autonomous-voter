5f65487c75b915ffe7bb1234ee320d83993a8e04
Khoa Phan <kp@conclave.dev>
2020-08-17 13:22:17 -0400

    Add setElection method

--- contracts/modules/MVoting.sol
+++ contracts/modules/MVoting.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.5.8;
 
 import "../celo/common/libraries/AddressLinkedList.sol";
+import "../celo/governance/interfaces/IElection.sol";
 
-contract VoteManagement {
+contract MVoting {
     using AddressLinkedList for LinkedList.List;
 
     struct Group {
@@ -19,9 +20,18 @@ contract VoteManagement {
         uint256 placed;
     }
 
+    IElection election;
     Group[] groupAllocations;
     LinkedList.List public vaults;
     address manager;
+
+    /**
+     * @notice Sets the interface for the Celo Election contract
+     * @param election_ Celo Election address
+     */
+    function setElection(address election_) internal {
+        election = IElection(election_);
+    }
 
     /**
      * @notice Sets the allocation for Celo election groups
@@ -32,5 +42,7 @@ contract VoteManagement {
     function setGroupAllocations(
         uint256[] calldata groupIndexes,
         uint256[] calldata allocations
-    ) external {}
+    ) public {
+
+    }
 }
