eb18e82f5725b3d449786157e5799ff6d5e0ca5c
Eko Mirhard <eko.mirhard@gmail.com>
2020-08-27 02:18:57 +0700

    Update analyze.sh

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -8,6 +8,6 @@ echo "Starting analyzer using Slither"
 
 docker run -it --name toolbox -d -v $(pwd):/share trailofbits/eth-security-toolbox
 
-docker exec toolbox bash -t -c "solc-select 0.5.8 && cd /share && truffle compile --all && slither . --ignore-compile && exit 1"
+docker exec toolbox bash -t -c "solc-select 0.5.8 && cd /share && rm build/contracts/* && truffle compile --all && slither . --ignore-compile && exit 1"
 
 docker rm toolbox -f
