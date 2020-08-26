538eb070220dcc68aebe4da035cccc7618eda676
Eko Mirhard <eko.mirhard@gmail.com>
2020-08-27 02:13:36 +0700

    Create analyze.sh

---
+++ scripts/analyze.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Initializing Eth-Security-Toolbox"
+
+docker pull trailofbits/eth-security-toolbox
+
+echo "Starting analyzer using Slither"
+
+docker run -it --name toolbox -d -v $(pwd):/share trailofbits/eth-security-toolbox
+
+docker exec toolbox bash -t -c "solc-select 0.5.8 && cd /share && truffle compile --all && slither . --ignore-compile && exit 1"
+
+docker rm toolbox -f
