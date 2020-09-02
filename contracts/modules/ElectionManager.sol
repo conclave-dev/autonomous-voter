46f44d54d4739ae47a803b4b9769b84fe0b1cece
Khoa Phan <kp@conclave.dev>
2020-09-02 19:19:34 -0400

    Add voter balance minimum and election group-setting

--- contracts/modules/ElectionManager.sol
+++ contracts/modules/ElectionManager.sol
@@ -16,25 +16,50 @@ contract ElectionManager {
     // Details about a group that will receive votes
     struct Group {
         // Index of the eligible Celo election group
-        uint256 groupIndex;
+        uint256 index;
         // Percent of total votes allocated to the group
-        uint256 percentOfTotal;
+        uint256 allocation;
         // Number of votes that have been placed for the group
         uint256 voted;
     }
 
-    LinkedList.List public managedAccounts;
     Votes public votes;
-    mapping(uint256 => Group[]) allocations;
+    mapping(uint256 => Group[]) groups;
+    uint256[] public groupCycles;
+    LinkedList.List public voters;
+    uint256 public minimumVoterBalance;
 
     /**
-     * @notice Adds msg.sender as a managed account
+     * @notice Adds a voter
      */
-    function addManagedAccount() external {
-        require(
-            managedAccounts.contains(msg.sender) == false,
-            "Account exists"
-        );
-        managedAccounts.push(msg.sender);
+    function addVoter() external {
+        require(voters.contains(msg.sender) == false, "Account exists");
+        voters.push(msg.sender);
+    }
+
+    /**
+     * @notice Sets the minimum voter balance requirement
+     */
+    function setMinimumVoterBalance(uint256 min) external {
+        minimumVoterBalance = min;
+    }
+
+    /**
+     * @notice Adds a set of election group and associated cycle to the `groups` mapping
+     * @dev It is up to the inheriting contract to validate
+     */
+    function _setGroups(
+        uint256[] memory groupIndexes,
+        uint256[] memory groupAllocations,
+        uint256 cycle
+    ) internal {
+        Group[] storage group = groups[cycle];
+
+        for (uint256 i = 0; i < groupIndexes.length; i += 1) {
+            group.push(Group(groupIndexes[i], groupAllocations[i], 0));
+        }
+
+        groups[cycle] = (group);
+        groupCycles.push(cycle);
     }
 }
--- contracts/modules/ElectionVoter.sol
+++ contracts/modules/ElectionVoter.sol
@@ -8,11 +8,11 @@ contract ElectionVoter {
     ElectionManager electionManager;
 
     /**
-     * @notice Sets the ElectionManager contract and allows it to manage votes
+     * @notice Sets the ElectionManager contract, allowing it to manage votes
      * @param electionManager_ Contract address
      */
     function _setElectionManager(address electionManager_) internal {
         electionManager = ElectionManager(electionManager_);
-        electionManager.addManagedAccount();
+        electionManager.addVoter();
     }
 }
